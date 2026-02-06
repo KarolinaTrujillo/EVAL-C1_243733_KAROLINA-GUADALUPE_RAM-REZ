3bc1647e7a468ea3ace6cbbb2bc47f2fce25e624
KarolinaTrujillo <243733@ids.upchiapas.edu.mx>
2026-02-06 09:31:37 -0600

    Ultimos ajustes de indexes.sql

--- db/indexes.sql
+++ db/indexes.sql
@@ -1,8 +1,14 @@
-CREATE INDEX idx_loans_copy_id
-ON loans(copy_id);
 
-CREATE INDEX idx_loans_due_at
-ON loans(due_at);
+CREATE INDEX idx_loans_copy_id ON loans(copy_id);
 
-CREATE INDEX idx_loans_member_id
-ON loans(member_id);
+
+CREATE INDEX idx_loans_due_at ON loans(due_at);
+
+
+CREATE INDEX idx_loans_member_id ON loans(member_id);
+
+
+CREATE INDEX idx_fines_loan_id ON fines(loan_id);
+
+
+CREATE INDEX idx_copies_book_id ON copies(book_id);
